236630e3e2e760f3f59bbc4758b0ff0ad8fa6352
Kreal11 <kyrylo.shyrokov.dev@gmail.com>
2024-05-25 18:27:56 +0300

    added e2e testing for signup request
    + added another sqlite DB especially for testing with ConfigService and ConfigModule
    + set NODE_ENV via cross-env for dev and test modes

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,30 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication System', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a signup request', () => {
+    const email = 'test11@test.com';
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'password' })
+      .expect(HttpStatus.CREATED)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(email);
+      });
+  });
+});
